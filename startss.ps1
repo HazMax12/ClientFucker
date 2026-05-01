ec485a84fe9d662659cf998cc1474eadf2e3e68b
HazMax12 <harrymacmillan@outlook.com>
2026-05-01 23:39:36 +0100

    Update startss.ps1

--- startss.ps1
+++ startss.ps1
@@ -1,10 +1,10 @@
-Start-Process powershell -ArgumentList "-ExecutionPolicy Bypass -NoExit -Command `"Invoke-Expression (Invoke-RestMethod 'https://raw.githubusercontent.com/zedoonvm1/powershell-scripts/refs/heads/main/DoomsDayDetector.ps1)`""
+Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -NoExit -Command "Invoke-RestMethod https://raw.githubusercontent.com/zedoonvm1/powershell-scripts/refs/heads/main/DoomsDayDetector.ps1 | Invoke-Expression"'
 
-Start-Process powershell -ArgumentList "-ExecutionPolicy Bypass -NoExit -Command `"Invoke-Expression (Invoke-RestMethod 'https://raw.githubusercontent.com/praiselily/lilith-ps/refs/heads/main/Services.ps1')`""
+Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -NoExit -Command "Invoke-RestMethod https://raw.githubusercontent.com/praiselily/lilith-ps/refs/heads/main/Services.ps1 | Invoke-Expression"'
 
-Start-Process powershell -ArgumentList "-ExecutionPolicy Bypass -NoExit -Command `"Invoke-Expression (Invoke-RestMethod 'https://raw.githubusercontent.com/MeowTonynoh/MeowModAnalyzer/main/MeowModAnalyzer.ps1')`""
+Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -NoExit -Command "Invoke-RestMethod https://raw.githubusercontent.com/MeowTonynoh/MeowModAnalyzer/main/MeowModAnalyzer.ps1 | Invoke-Expression"'
 
-Start-Process powershell -ArgumentList "-ExecutionPolicy Bypass -NoExit -Command `"Invoke-Expression (Invoke-RestMethod 'https://raw.githubusercontent.com/JavaXYZZ/ScreenSharing/main/McTools.ps1')`""
+Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -NoExit -Command "Invoke-RestMethod https://raw.githubusercontent.com/JavaXYZZ/ScreenSharing/main/McTools.ps1 | Invoke-Expression"'
 
 Start-Process "shell:recent"
 Start-Process $env:TEMP
